d9b4aeacacc9775523c7db5479e7788554c00257
Benita Priyanka Isaac <benita@Isaacs-2021-MacBook-Pro.local>
2024-03-16 16:12:39 -0700

    adding code into thought model

---
+++ models/Thought.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+        }
+    }
+);
+
+//include virtual 
