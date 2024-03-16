e6ce9d6c8c1539b02d2834817bac97ef3b93b238
Benita <benita.isaac@gmail.com>
2024-03-16 16:36:43 -0700

    added reactions schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,29 @@
 const {Schema, model} = require('mongoose');
-const reactionSchema = require('./Reaction');
+// const reactionSchema = require('./Reaction');
 
+//add reaction schema 
+const reactionSchema = new Schema({
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  });
+  
+//add thought schema 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
