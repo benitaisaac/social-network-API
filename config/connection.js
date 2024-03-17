728c093edda64cd344e65f0d11fb14eb1d73db78
Benita <benita.isaac@gmail.com>
2024-03-17 12:55:25 -0700

    changed DB name in server file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection} = require('mongoose');
 
 //I might need to change this to mongodb atlas
-const connectionString = 'mongodb://127.0.0.1:27017/studentsDB';
+const connectionString = 'mongodb://127.0.0.1:27017/socialMediaDB';
 
 connect(connectionString);
 
