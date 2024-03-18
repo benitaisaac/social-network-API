c7d2f8ce626171d87ac6928f85387d2d6f2ff625
Benita <benita.isaac@gmail.com>
2024-03-17 21:50:13 -0700

    fixed the thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,41 +1,38 @@
-const {Schema, model} = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
-  
-//add thought schema 
+//add thought schema
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            min_length: 1,
-            max_length: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        username: {
-            type: Schema.Types.ObjectId,
-            ref: 'user',
-            required: true,
-        },
-        reactions: [reactionSchema]
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //TODO: getter method 
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  }
+  // {
+  //     toJSON: {
+  //         getters: true,
+  //     },
+  // }
 );
 
-//TODO: include virtual 
-thoughtSchema
-.virtual('reactionCount')
-.get(function (){
-    return this.reactions.length;
+//TODO: include virtual
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 //create model for collection
-const Thought = model('thought', thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 module.exports = Thought;
