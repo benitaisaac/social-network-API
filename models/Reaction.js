e38c5e65fe947f8381c97d866897337e8bc53aab
Benita <benita.isaac@gmail.com>
2024-03-17 17:01:23 -0700

    moved reaction schema to its own folder

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,7 +1,25 @@
 const {Schema, model} = require('mongoose');
 
-const reactionSchema = new Schema(
-    {
-        
+//add reaction schema 
+const reactionSchema = new Schema({
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     }
-)
+  });
+
+  module.exports = reactionSchema;
