441335f203952e2aad612758c3600566310d54c1
Benita <benita.isaac@gmail.com>
2024-03-17 17:02:47 -0700

    added comments and todos

---
+++ utils/data.js
@@ -0,0 +1,8 @@
+//TODO: create an array of names? 
+
+//TODO: create an array of 'thoughts' 
+
+//TODO: create a random index function (use random pick from data.js)
+//TODO: create a function to get a random user
+//TODO: create a function to get a random reaction 
+//TODO: createa a function to get a random thought
