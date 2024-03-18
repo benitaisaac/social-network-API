b0a317c22704caae8858a82981b318699a32eef2
Benita <benita.isaac@gmail.com>
2024-03-17 17:03:18 -0700

    started adding code to seeding file

--- utils/seed.js
+++ utils/seed.js
@@ -1,2 +1,30 @@
 const connection = require('../config/connection');
-const { Reaction , User} = require('../models');
+const { Thought , User} = require('../models');
+
+//An error event listener is set up on the connection object to handle any errors.
+connection.on('error', (err) => err);
+
+connection.once('open', async() => {
+    console.log("connected");
+//Delete collections if they exist 
+    //create a user variable to check if there is data in the database
+    let userCheck = await connection.db.listCollections({ name: 'users'}).toArray();
+    // if there is anything in the array, it will delete the collection
+    if(userCheck.length){
+        //note that mongoDB will make the collection name plural 
+        await connection.dropCollection('users');
+    }
+
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts'}).toArray();
+    //if there are any elements in the thought array, delete the collection
+    if(thoughtCheck.length){
+        await connection.dropCollection('thoughts');
+    }
+
+    //create an empty array to hold users
+    const users = [];
+    const thoughts = [];
+
+
+})
+
