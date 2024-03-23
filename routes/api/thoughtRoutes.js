d440b0c1f434cae3ae958835b8246677cfb04a6d
Benita <benita.isaac@gmail.com>
2024-03-23 15:49:44 -0700

    added route for getting a thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 //import routes from controllers file 
 
-const {getThoughts, testThoughts} = require('../../controllers/thoughtController');
+const {getThoughts, getSingleThought} = require('../../controllers/thoughtController');
 
 
 // /api/thoughts
 router.route('/').get(getThoughts);
 
-router.route('/test').get(testThoughts);
+router.route('/:thoughtId').get(getSingleThought);
 
 module.exports = router;
