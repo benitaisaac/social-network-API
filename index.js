2bf759de7e83c32caea71e2c531191ac5c469301
Benita <benita.isaac@gmail.com>
2024-03-23 11:57:03 -0700

    changed 'once' to 'open'

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routes = require('./routes');
 //what does this code do? 
 // const cwd = process.cwd();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
 
-db.once('once', () => {
+db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API running on port ${PORT}`);
     });
