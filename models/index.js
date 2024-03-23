8e92391cc56f91cdc05bd98db13ef31c71d0a161
Benita <benita.isaac@gmail.com>
2024-03-23 14:26:11 -0700

    added Thought model to index

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const Reaction = require('./Reaction');
 const User = require('./User');
-//Note that the Thoughts model is included within Users 
-module.exports= {Reaction, User};
+const Thought = require('./Thought');
+
+
+module.exports= {Reaction, User, Thought};
