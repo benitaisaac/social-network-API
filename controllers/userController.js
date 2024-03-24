dc015b1f23e5a3670dd4e94c2d0c4ec2616b6558
Benita <benita.isaac@gmail.com>
2024-03-24 11:31:45 -0700

    added get all users route

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+
+module.exports = {
+    async getUsers (req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    }
+
+}
