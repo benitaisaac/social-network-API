febd01432b7743ea996a7c847d4bb933b5afbbfb
Benita <benita.isaac@gmail.com>
2024-03-27 18:56:26 -0700

    added deleteFriend function

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,23 +71,43 @@ module.exports = {
     }
   },
 
-  // add a friend to a user's friend list 
+  // add a friend to a user's friend list
   async addFriend(req, res) {
     const { userId, friendId } = req.params;
     try {
-        const user = await User.findOneAndUpdate(
-            // get the user that you want to update
-            { _id: userId},
-            // add value to an array, it must be unique. 
-            //friends: friendId specifies the field 'friends' in the document 
-            { $addToSet: {friends: friendId}},
-            { new: true}
-        );
+      const user = await User.findOneAndUpdate(
+        // get the user that you want to update
+        { _id: userId },
+        // add value to an array, it must be unique.
+        //friends: friendId specifies the field 'friends' in the document
+        { $addToSet: { friends: friendId } },
+        { new: true }
+      );
 
-        if(!user){
-            res.status(404).json({message: "no user with this id exists!"})
-        }
-        res.json(user);
+      if (!user) {
+        res.status(404).json({ message: "no user with this id exists!" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //delete a friend 
+  async deleteFriend(req, res) {
+    const { userId, friendId } = req.params;
+    try {
+      const user = await User.findOneAndDelete(
+        { _id: userId },
+        { $pull: { friends: friendId } },
+        { new: true }
+      );
+
+      if (!user) {
+        res.status(404).json({ message: "no user with that Id found!" });
+      }
+
+      res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
