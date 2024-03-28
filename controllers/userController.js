128b6ca101873a61e97e6c695e8bfda5ad60c5c2
Benita <benita.isaac@gmail.com>
2024-03-27 17:47:01 -0700

    added functions to update and delete a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,7 @@ module.exports = {
         }
     },
 
-//TODO: POST  new user
+// function to create a new user
     async createUser (req, res) {
         try {
             const user = await User.create(req.body);
@@ -35,11 +35,41 @@ module.exports = {
         } catch(err) {
             res.status(500).json(err);
         }
+    },
+
+    //update a user by its _id
+    async updateUser (req, res) {
+     try {
+        const user = await User.findOneAndUpdate(
+            {_id: req.params.userId},
+            { $set: req.body},
+            //runValidators makes sure that the values that are entered in the request are validated by Mongoose, throught the schema 
+            //new ensures that the method returns the updated document after update is completed
+            {runValidators: true, new: true}
+        );
+
+        if(!user) {
+            res.status(404).json({ message: 'No user with this id!'});
+        }
+        res.json(user);
+     }  catch(err) {
+        res.status(500).json(err);
+     } 
+    },
+
+    //DELETE to remove a user by its _id
+    async deleteUser (req, res) {
+        try {
+            const user = await User.findOneAndDelete({_id: req.params.userId});
+
+            if(!user){
+                res.status(404).json({message: 'No user with that id!'});
+            }
+            res.json({message: "user deleted!"})
+
+        } catch(err) {
+            res.status(500).json(err);
+        }
     }
-
-
-    //TODO: PUT to update a user by its _id
-
-    //TODO: DELETE to remove a user by its _id
 
 }
